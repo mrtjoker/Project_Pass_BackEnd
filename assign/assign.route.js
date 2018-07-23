ea99c8f87ed900c8a3e42a042a3b130781c17abe
ford <ftn2539@gmail.com>
2018-07-23 18:39:40 +0700

    store get assign

--- assign/assign.route.js
+++ assign/assign.route.js
@@ -7,6 +7,7 @@ router.get( '/assignId/:id', controller.findAssignId );
 router.get( '/assignnId/:id', controller.projectProgress );
 router.post('/assign',controller.add); // call function add in controller  
 router.put( "/assign/:id", controller.update );
+router.put( "/assignn/:id", controller.updateMat );
 router.delete( "/assign/:id", controller.destroy );
 
 module.exports = router // export module for use router in another files
